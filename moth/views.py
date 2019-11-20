738edd1dd5b92f5b45dd232b2fbb2e8a4c511fb4
daltonhart <daltonvaren@gmail.com>
2019-11-20 15:06:26 -0800

    adds base home and service route

--- moth/views.py
+++ moth/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse, JsonResponse
+from .models import Service
 # Create your views here.
 
 
 def landing(request):
     return render(request, 'landing.html')
+
+
+def api_services(request):
+    services = Service.objects.all()
+    data = []
+    for service in services:
+        data.append({
+            "name": service.name,
+            "price": service.price,
+            "description": service.description, "service_type": service.service_type})
+    return JsonResponse({"data": data, "status": 200})
